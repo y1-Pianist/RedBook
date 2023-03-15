2a2644de8ad69be04c32af75a2ce899b69f83f25
liangliangdeda <2690373236@qq.com>
2023-03-15 13:21:52 +0800

    Initial Commit

---
+++ RedBook/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RedBook
+//
+//  Created by 亮亮的大 on 2023/3/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
